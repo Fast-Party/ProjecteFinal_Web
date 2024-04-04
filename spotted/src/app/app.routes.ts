a41bcb36f50d9f082b4bec6aa95d6657c10d5006
Dylan Anderson Leiva Maldonado <100689619+maldonadodyllei@users.noreply.github.com>
2024-04-04 20:19:10 +0200

    Configuraciones de archivos de ruta y creacion de archivos landing page

--- spotted/src/app/app.routes.ts
+++ spotted/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes } from '@angular/router';
+import { LandingPageComponent } from './modules/landing-page/pages/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'landing-page', component: LandingPageComponent},
+    
+    {path: '', redirectTo: 'landing-page', pathMatch: 'full'}
+];
+
