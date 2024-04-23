f98fb8e46a454284e2796b57d9a3dd3364240ad9
pauporbla <pauporbla@campus.monlau.com>
2024-04-23 20:09:31 +0200

    upt

--- spotted/src/app/modules/create-plan-page/page/create-plan-page/create-plan-page.component.ts
+++ spotted/src/app/modules/create-plan-page/page/create-plan-page/create-plan-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { PlanModel } from '../../../../models/plan.model';
 
 @Component({
   selector: 'app-create-plan-page',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, FormsModule],
   templateUrl: './create-plan-page.component.html',
   styleUrl: './create-plan-page.component.scss'
 })
@@ -17,7 +18,8 @@ export class CreatePlanPageComponent {
     this.plan = new PlanModel();
   }
 
-  fetchPlanes() {
+  postPlan() {
+    console.log(this.plan)
     try {
       this.http.post('http://localhost:3000/postPlan', this.plan).subscribe((res: any) => {
         console.log('hola');
