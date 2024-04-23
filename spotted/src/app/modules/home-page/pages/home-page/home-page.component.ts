0ff25e7420f3c5648efa78882fcaf0fac080dadf
pauporbla <pauporbla@campus.monlau.com>
2024-04-23 19:46:06 +0200

    upt home

--- spotted/src/app/modules/home-page/pages/home-page/home-page.component.ts
+++ spotted/src/app/modules/home-page/pages/home-page/home-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
-import { Router } from '@angular/router';
 import { PlanModel } from '../../../../models/plan.model';
 
 @Component({
