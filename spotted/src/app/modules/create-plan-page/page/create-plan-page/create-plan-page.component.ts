719f307f6bd6ab831f2bbd8f95498b6d385b728a
pauporbla <pauporbla@campus.monlau.com>
2024-04-24 17:58:32 +0200

    upt

--- spotted/src/app/modules/create-plan-page/page/create-plan-page/create-plan-page.component.ts
+++ spotted/src/app/modules/create-plan-page/page/create-plan-page/create-plan-page.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { PlanModel } from '../../../../models/plan.model';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-create-plan-page',
@@ -21,7 +22,10 @@ export class CreatePlanPageComponent {
   postPlan() {
     console.log(this.plan)
     try {
-      this.http.post('http://localhost:3000/postPlan', this.plan).subscribe((res: any) => {
+      this.http.post('http://localhost:3000/postPlan', this.plan).pipe(catchError(error => {
+        console.log('error is: ', error);
+        return error;
+      })).subscribe((res: any) => {
         console.log('hola');
         if (res) {
           console.log("plan posted correctly");
