9cb9e71a8adad26534cb750e40c70f5349fd801b
Dylan Anderson Leiva Maldonado <100689619+maldonadodyllei@users.noreply.github.com>
2024-05-07 20:24:45 +0200

    Updt

--- spotted/src/app/modules/register-page/pages/register-page/register-page.component.ts
+++ spotted/src/app/modules/register-page/pages/register-page/register-page.component.ts
@@ -10,13 +10,13 @@ import { catchError } from 'rxjs';
   standalone: true,
   imports: [FormsModule, HttpClientModule, RouterLink],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.scss'
+  styleUrl: './register-page.component.scss',
 })
 export class RegisterPageComponent {
   registerObj: Register;
   showPassword: boolean = false;
   applyAnimationEye: boolean = false;
-
+  nextForm: boolean = true;
   constructor(private http: HttpClient, private router: Router) {
     this.registerObj = new Register();
   }
@@ -24,28 +24,40 @@ export class RegisterPageComponent {
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
     this.applyAnimationEye = true;
-}
+  }
+
+  gestionBoton() {
+    if (this.nextForm) {
+      this.nextForm = !this.nextForm;
+    } else {
+      this.onregister();
+    }
+  }
 
   onregister() {
     try {
-      this.http.post('http://localhost:3000/registrarUsuario', this.registerObj).pipe(catchError(error => {
-        console.log('error is: ', error);
-        if (error.status === 400) {
-          window.alert('There are fields that must be filled')
-        }
-        return error;
-      })).subscribe((res: any) => {
-        console.log(res['message']);
-        if (res['message'] === "User registered correctly.") {
-          this.router.navigateByUrl('/landing-page')
-        } else {
-          console.log('error 401')
-        }
-      })
+      this.http
+        .post('http://localhost:3000/registrarUsuario', this.registerObj)
+        .pipe(
+          catchError((error) => {
+            console.log('error is: ', error);
+            if (error.status === 400) {
+              window.alert('There are fields that must be filled');
+            }
+            return error;
+          })
+        )
+        .subscribe((res: any) => {
+          console.log(res['message']);
+          if (res['message'] === 'User registered correctly.') {
+            this.router.navigateByUrl('/landing-page');
+          } else {
+            console.log('error 401');
+          }
+        });
     } catch (error) {
-      return console.log('sdfd')
+      return console.log('sdfd');
     }
-
   }
 }
 
