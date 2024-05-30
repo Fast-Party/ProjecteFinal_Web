82d8721f512ed73d0bb4c75a639972e081a512cf
Dylan Anderson Leiva Maldonado <100689619+maldonadodyllei@users.noreply.github.com>
2024-05-30 20:27:20 +0200

    Updt + falta actualizar datos

--- spotted/src/app/modules/profile-page/pages/profile-page/profile-page.component.ts
+++ spotted/src/app/modules/profile-page/pages/profile-page/profile-page.component.ts
@@ -6,6 +6,7 @@ import { ProfileModel } from '../../../../models/profile.model';
 import { PlanCardModel } from '../../../../models/plan.model';
 
 import { UserService } from '../../../../api/services/user.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-page',
@@ -15,25 +16,33 @@ import { UserService } from '../../../../api/services/user.service';
   styleUrl: './profile-page.component.scss'
 })
 export class ProfilePageComponent implements OnInit {
-
-  profileContainer = 'profile-container';
-  planContainer = 'plan-container';
-
+  UpdateForm!: FormGroup;
+  
   idUsuario?: number;
   perfilUsuario: ProfileModel;
   planesUsuario: PlanCardModel[] = [];
+  nombreUsuarioOnly: string = '';
+  apellidosUsuarioOnly: string = '';
+  originalDate: any;
+  dia: number = 0;
+  mes: number = 0;
+  year: number = 0;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private userService: UserService) {
+  daysArray: number[] = Array.from({ length: 31 }, (_, i) => i + 1);
+  yearsArray: number[] = Array.from({ length: 85 }, (_, i) => new Date().getFullYear() - 16 - i);
+  isAgeValid: boolean = true;
+
+  isEditing: boolean = false;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient, private userService: UserService, private fb : FormBuilder) {
     this.perfilUsuario = new ProfileModel();
   }
 
   ngOnInit(): void {
 
     this.idUsuario = this.userService.getIdUsuario();
-    console.log(this.idUsuario);
     
     if (this.idUsuario != null) {
-      console.log(this.idUsuario)
       const body = { IdUsuario: this.idUsuario }
       try {
         this.http.post('http://localhost:3000/perfilUsuario', body).pipe(catchError(error => {
@@ -44,7 +53,17 @@ export class ProfilePageComponent implements OnInit {
             this.perfilUsuario = res.perfil[0];
             console.log("perfil", this.perfilUsuario);
             this.planesUsuario = res.planes;
-            console.log(this.planesUsuario);
+            this.nombreUsuarioOnly = this.perfilUsuario.Nombre.split(' ')[0];
+            this.apellidosUsuarioOnly = this.perfilUsuario.Nombre.split(' ').slice(1).join(' ');
+
+            this.originalDate = this.perfilUsuario.FechaNacimiento;
+            const date = new Date(this.originalDate);
+            this.dia = date.getDate();
+            this.mes = date.getMonth();
+
+            console.log('mes', this.mes);
+            this.year = date.getFullYear();
+            console.log('planes', this.perfilUsuario.FechaNacimiento);
           } else {
             console.log('couldnt post plan')
           }
